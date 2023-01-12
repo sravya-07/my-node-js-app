17a4a0d700f707d4848ad7f051638893c5cbbc9b
sravya-07 <92778258+sravya-07@users.noreply.github.com>
2023-01-12 18:06:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
       </head>
       <body>
         <section>
-          <h2>Sharon Course Goal</h2>
+          <h2>Sravya Course Goal</h2>
           <h3>${userGoal}</h3>
         </section>
         <form action="/store-goal" method="POST">
